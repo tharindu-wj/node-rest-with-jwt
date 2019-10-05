d3452eda2b0d8d5367e6d9decd867fd5e390769b
TharinduW <tharinduwjh@gmail.com>
2019-10-06 00:13:41 +0530

    express middlewares added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv')
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
+const helmet = require('helmet')
+
+const app = express();
 
 // Import Routes
 const authRoute = require('./src/routes/auth');
@@ -12,16 +14,18 @@ dotenv.config();
 // Connect to DB
 mongoose.connect(
     process.env.DB_CONNECT,
-    {useNewUrlParser: true, useUnifiedTopology: true},
-    ()=> console.log('Connected to DB')
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected to DB')
 )
 
 // Middleware
+app.use(helmet())
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 // Route Middlewares
 app.use('/api/user', authRoute);
 app.use('/api/posts', postsRoute);
 
-app.listen(3000, () =>  console.log('Server up and running'));
+app.listen(3000, () => console.log('Server up and running'));
